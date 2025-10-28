7ef92c7177da9b21f1b0a781594c9aaf7d0133c6
JOVANNIZ10 <ipriala37@gmail.com>
2025-10-28 08:48:45 +0100

    generate teams implementation

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ form.onsubmit=function(event){
         mensaje.style.display="block"
         return;
     }
-   
+    
     let numeroEquipos=teamCount.value;
     teamMatesArray = participants.value.split('\n').filter(name => name.trim() !== "");
     if(teamMatesArray.length==0){
@@ -20,26 +20,42 @@ form.onsubmit=function(event){
         mensaje.style.display="block"
         return;
     }
+    
     if(teamMatesArray.length<numeroEquipos){
         mensaje.innerText="El número de equipos no puede ser mayor que el número de participantes."
         mensaje.style.display="block"
         return;
     }
+
     mensaje.style.display="none"
-    let ul = document.createElement("ul");
-    ul.id="teamMatesList";
-    teamMatesArray.forEach(name => {
-        let li = document.createElement("li");
-        li.innerText = name;
-        ul.appendChild(li);
-    });
     let resultsContainer = document.getElementById("results-container");
-    resultsContainer.appendChild(ul);
+    for(let i=0; i<numeroEquipos; i++){
+        let ul= document.createElement("ul");
+        ul.id="team"+(i+1);
+        resultsContainer.appendChild(ul);
+        let liName=document.createElement("li");
+        liName.innerText="Equipo "+(i+1);
+        document.getElementById("team"+(i+1)).appendChild(liName);
+    }
+    // Repartir los nombres aleatoriamente entre los equipos
+    while(teamMatesArray.length > 0){
+        for(let i=0; i<numeroEquipos; i++){
+            if(teamMatesArray.length === 0) break;
+            let randomIndex = Math.floor(Math.random() * teamMatesArray.length);
+            let selectedName = teamMatesArray.splice(randomIndex, 1)[0];
+            let li = document.createElement("li");
+            li.innerText = selectedName;
+            document.getElementById("team"+(i+1)).appendChild(li);
+        }
+    }
     resultsContainer.style.display="block";
+    
+    let ulFirstTeam=document.getElementById("team1");
+    ulFirstTeam.children[0].style.borderTopLeftRadius="20px";
+    ulFirstTeam.children[0].style.borderTopRightRadius="20px";
 }
 
 teamCount.addEventListener("change", function(){
     mensaje.style.display="none"
     equiposSeleccionados=true;
 })
-
